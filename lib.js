27613a0bd1cbef46455e157375c0723de540cb77
cablesalty <cablesalty@gmail.com>
2024-03-08 23:55:05 +0100

    Some changes

--- lib.js
+++ lib.js
@@ -1,4 +1,10 @@
 
 
-// Get all custom elements
-console.log(document.getElementsByTagName("dypanel"))
+// Get all content elements inside panels
+var panelElements = document.getElementsByTagName("content");
+console.log(panelElements.length);
+
+for (var i = 0; i < panelElements.length - 1; i++) {
+    console.log("Adding division between element: " + panelElements[i]);
+    panelElements[i].outerHTML += "<br><hr><br>";
+}
