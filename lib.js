b90bcd9be15e785b935687133e49afd4139f6e75
cablesalty <cablesalty@gmail.com>
2024-03-09 01:28:00 +0100

    Last commit for today

--- lib.js
+++ lib.js
@@ -1,15 +1,37 @@
 
 
 // Get all panels
-var panelElements = document.getElementsByTagName("panel");
+let panelElements = document.getElementsByTagName("panel");
 console.log("Panels: " + panelElements.length);
 
-for (var i = 0; i < panelElements.length; i++) {
+for (let i = 0; i < panelElements.length; i++) {
     // get all contents inside panels
-    var contentElements = panelElements[i].getElementsByTagName("content");
+    let contentElements = panelElements[i].getElementsByTagName("content");
+    console.log("Contents in panel " + panelElements[i] + ": " + panelElements.length)
 
-    for (var i = 0; i < contentElements.length - 1; i++) {
+    for (let i = 0; i < contentElements.length - 1; i++) {
         console.log("Adding seperators to: " + contentElements[i]);
         contentElements[i].outerHTML += "<br><hr><br>";
+    }
+}
+
+// Get navbar
+let menubarElements = document.getElementsByTagName("menubar");
+console.log("Menubars: " + menubarElements.length);
+
+
+for (let i = 0; i < menubarElements.length; i++) {
+    let menubarElement = menubarElements[i];
+    console.log(menubarElement);
+
+    let menubarPlacement = menubarElement.getAttribute("placement");
+    console.log(menubarPlacement);
+    console.log(menubarElement.scrollHeight);
+    if (menubarPlacement == "top") {
+        document.getElementsByTagName("body")[1].style.marginTop += menubarElement.scrollHeight;
+    } else if (menubarPlacement == "bottom") {
+        document.querySelector("body").style.marginBottom = menubarElement.scrollHeight;
+    } else {
+        console.error("DynamoJS: Invalid placement of a menubar!")
     }
 }
